7ddde053b44f865ec3d9df7858b9503bed15e44a
EC2 Default User <ec2-user@ip-172-31-11-21.us-east-2.compute.internal>
2018-02-07 05:16:39 +0000

    Almost done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,50 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  res = 0
+  arr.each {|x| res +=x.to_i}
+  res
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
-end
+  res=0
+  m1 = arr.max.to_i
+  arr.delete_at(arr.rindex(m1).to_i)
+  m2=arr.max.to_i
+  res = m1+m2
+end 
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in (0...arr.length)
+    for j in (i+1...arr.length)
+      if arr[i]+arr[j]==n 
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[^aeiou]/i.match?(/^\w/.match(s).to_s) 
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if !(/^(1|0)+$/.match?(s))
+    return false
+  end
+  s.to_i(2)%4==0
 end
 
 # Part 3
-
 class BookInStock
 # YOUR CODE HERE
 end
+
+
